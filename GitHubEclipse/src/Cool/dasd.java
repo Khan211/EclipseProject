2113aef2bfc1747cde0404e9c62a47032c299fb9
Khan211 <wariskhan745@gmail.com>
2022-02-10 01:44:56 -0500

    XcZc

--- GitHubEclipse/src/Cool/dasd.java
+++ GitHubEclipse/src/Cool/dasd.java
@@ -5,7 +5,14 @@ public class dasd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Khan");
+		System.out.println("NO");		
+		
+		
+		
 
 	}
-
+	public void methodD() {
+		
+		//Khan 
+	}
 }
